d3fe14dbf948ecde1b83240a1dcb3a8f79d41f82
Arthur Moraes do Lago <arthurmoraeslago@gmail.com>
2018-11-23 00:47:32 -0200

    added some comentaries

--- arduino.ino
+++ arduino.ino
@@ -68,7 +68,6 @@ volatile unsigned long int timeout_count;
 
 ISR(TIMER3_COMPA_vect){
 	if (recv_count != expected_recv){
-		/*Serial.println("t");*/
 		timeout_count++;
 		tx_buffer[tx_buffer_start].sent = 0;
 	}
@@ -87,7 +86,10 @@ ISR(TIMER3_COMPA_vect){
 	
 }
 
+// Called when the register with the next byte to be sent is empty
+// (meanwhile the byte that was in the register before is being shifted out in the transmission register):
 ISR(USART3_UDRE_vect){
+	// Put the next byte in the register:
 	UDR3 = tx_buffer[tx_buffer_start].buff[tx_buffer[tx_buffer_start].sent];
 	tx_buffer[tx_buffer_start].sent++;
 	if (tx_buffer[tx_buffer_start].sent >= tx_buffer[tx_buffer_start].len){
@@ -96,6 +98,19 @@ ISR(USART3_UDRE_vect){
 	}
 }
 
+// Called when both the transmission register and the next byte register are empty, so transmission complete:
+ISR(USART3_TX_vect){
+	// turn off transmission, turn on reception:
+	UCSR3B &= ~(1 << 3);
+	UCSR3B |=  (1 << 4);
+
+	//Set an interrupt in case dynamixel fails to respond. The time until the interrupt is calculated to be bigger than
+	// the servo return delay time and some security margin:	
+	OCR3A = TCNT3 + 32000;// about 1ms
+	TIMSK3 |= 0x02;
+}
+
+// Called when a byte is received:
 ISR(USART3_RX_vect){
 	recv[recv_count] = UDR3;
 	// Set an interrupt to handle timeout if do not finish receiving all expected bytes to re-transmit the instruction:
@@ -142,16 +157,6 @@ ISR(USART3_RX_vect){
 
 }
 
-ISR(USART3_TX_vect){
-	// turn off transmission, turn on reception:
-	UCSR3B &= ~(1 << 3);
-	UCSR3B |=  (1 << 4);
-
-	//Set an interrupt in case dynamixel fails to respond. The time until the interrupt is calculated to be bigger than
-	// the servo return delay time and some security margin:	
-	OCR3A = TCNT3 + 32000;// about 1ms
-	TIMSK3 |= 0x02;
-}
 
 
 
@@ -208,11 +213,12 @@ int queueInstruction(unsigned char id, enum Dynamixel_Instruction inst, unsigned
 	
 }
 
+/*
+ * Function for stepping to the next buffer position, "removing" the sent command.
+ * It accounts for when the buffer is empty, jumping to the default command, or finishing
+ * to send a default command.
+ */
 void buffer_step(){
-	/*Serial.print(tx_buffer_size);*/
-	/*Serial.print("buffer clear");*/
-	/*Serial.println(tx_buffer_start);*/
-
 	if (tx_buffer_size == 0){
 		tx_buffer_start = 8;
 		tx_buffer[8].sent = 0;
@@ -231,14 +237,22 @@ void buffer_step(){
 	}
 }
 
+/*
+ * Callback function for telemetry command, which is also the comman sent while
+ * no other command is in queue.
+ * REMEMBER: this will be called from an interrupt so it must be brief!
+ */
 void telemetry_callback(unsigned char *recv){
-	/*Serial.print("a");*/
+	// Update current error and sticky error:
 	dyn_error = recv[4];
 	dyn_error_sticky |= dyn_error;
 
+	// Update present position:
 	*((uint8_t *)&present_position) = recv[5];
 	*(((uint8_t *)&present_position) + 1) = recv[6];
 
+	// Use the updated position to update the global position estimate, taking for the cases where the dynamixel is in its
+	// encoder dead zone:
 	if (last_position == 1023){
 		if (present_position < 128){
 			global_pos += DEAD_ZONE_ESTIMATE + (int)present_position;
@@ -260,12 +274,13 @@ void telemetry_callback(unsigned char *recv){
 		last_position = present_position;
 	}
 
-
+	// Upate present speed. This is also unreliable on encoder dead zone:
 	*((uint8_t *)&present_speed) = recv[7];
 	*(((uint8_t *)&present_speed) + 1) = recv[8];
 	if (present_speed & 0x400)
 		present_speed = -(present_speed & 0x3FF);
 
+	// Update current load:
 	*((uint8_t *)&present_load) = recv[9];
 	*(((uint8_t *)&present_load) + 1) = recv[10];
 	if (present_load & 0x400)
@@ -284,33 +299,45 @@ void telemetry_callback(unsigned char *recv){
 	#endif
 }
 
+/*
+ * Default callback for commands, just updates the error:
+ */
 void status_callback(unsigned char *recv){
-	/*Serial.println("b");*/
 	dyn_error = recv[4];
 	dyn_error_sticky |= dyn_error;
 }
 
 void setup() {
+	// Start communication with computer for logging:
+	// The baud rate is the fastest possible to avoid
 	Serial.begin(2000000);
 	
-	tx_buffer[8].buff[0] = 0xFF;
-	tx_buffer[8].buff[1] = 0xFF;
-	tx_buffer[8].buff[2] = 0x01;
-	tx_buffer[8].buff[3] = 0x04;
-	tx_buffer[8].buff[4] = 0x02;
-	tx_buffer[8].buff[5] = 36;
-	tx_buffer[8].buff[6] = 11;
-	tx_buffer[8].buff[7] = ~(0x01 + 0x04 + 0x02 + 36 + 11);
-	tx_buffer[8].len = 8;
-	tx_buffer[8].sent = 0;
-	tx_buffer[8].callback = &telemetry_callback;
+	// The command immediatly outside the buffer limits is sent whenever the bus is idle:
+	// We chose a command that returns almost all status variables:
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[0] = 0xFF;
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[1] = 0xFF;
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[2] = 0x01;
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[3] = 0x04;
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[4] = 0x02;
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[5] = 36;
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[6] = 11;
+	tx_buffer[MAX_TX_BUFFER_SIZE].buff[7] = ~(0x01 + 0x04 + 0x02 + 36 + 11);
+	tx_buffer[MAX_TX_BUFFER_SIZE].len = 8;
+	tx_buffer[MAX_TX_BUFFER_SIZE].sent = 0;
+	tx_buffer[MAX_TX_BUFFER_SIZE].callback = &telemetry_callback;
 	tx_buffer_start = 8;
 	tx_buffer_size = 0;
 
 
-	tx_buffer_start = 8;
-	tx_buffer_size = 0;
+	// Set up timer 3 to generate interrupts when requested, with the desired precision:
+	TCCR3A = 0;
+	TCCR3B = 1;	
+	// Disable timer interrupts for now:
+	TIMSK3 &= ~0x02;
 
+	// ubrr is calculated according to the desired baud rate:
+	// This sets the baud rate to 100k
+	// Table 22-1 from atmega2560 datasheet.
 	int ubrr = 9;
 	UBRR3H = (unsigned char)(ubrr>>8);
 	UBRR3L = (unsigned char)ubrr;
@@ -326,29 +353,23 @@ void setup() {
 	UCSR3B |=  (1 << 3);
 	UCSR3B |=  (1 << 5);
 
-	/*TCCR3A &= ~0xC0;*/
-	TCCR3A = 0;
-	TCCR3B = 1;
-	/*OCR3A = 600;*/
-	// Disable timer interrupts:
-	TIMSK3 &= ~0x02;
-
 	// Wait until we get the current position:
 	delay(500);
 	global_pos = present_position;
 	last_position = present_position;
 	delay(500);
 
+	// Enter wheel mode:
 	queueInstruction(1, DYN_WR, 3, 8, 0, 0, status_callback);
-	queueInstruction(1, DYN_WR, 3, 32, 50, 5, status_callback);
-	queueInstruction(1, DYN_WR, 3, 34, 240, 1, status_callback);
+	// set speed:
+	/*queueInstruction(1, DYN_WR, 3, 32, 50, 5, status_callback);*/
+	// set maximum torque:
+	queueInstruction(1, DYN_WR, 3, 34, 0xff, 3, status_callback);
 
 	// start moving:
 	/*queueInstruction(1, DYN_WR, 3, 30, 200, 0, status_callback);*/
 }
 
-unsigned long int last_action = millis();
-int fez = 1;
 
 void loop() {
 
